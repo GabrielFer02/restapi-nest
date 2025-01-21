e5a01b5d7ab17a710529947d83cb4f202f2b047d
Gabriel Fernando <gf313074@gmail.com>
2025-01-21 12:03:57 -0300

    create methods to create, find, update and remove

--- src/person/person.service.ts
+++ src/person/person.service.ts
@@ -1,4 +1,8 @@
-import { ConflictException, Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreatePersonDto } from './dto/create-person.dto';
 import { UpdatePersonDto } from './dto/update-person.dto';
 import { InjectRepository } from '@nestjs/typeorm';
@@ -12,7 +16,11 @@ export class PersonService {
     private readonly personRepository: Repository<Person>,
   ) {}
 
-  async create(createPersonDto: CreatePersonDto) {
+  notFoundException() {
+    throw new NotFoundException('Pessoa não eoncontrada');
+  }
+
+  create(createPersonDto: CreatePersonDto) {
     try {
       const personData = {
         email: createPersonDto.email,
@@ -21,30 +29,49 @@ export class PersonService {
       };
 
       const newPerson = this.personRepository.create(personData);
-      await this.personRepository.save(newPerson);
+      return this.personRepository.save(newPerson);
 
-      return newPerson;
+      // return newPerson;
     } catch (error) {
-      if (error.code === '23505') {
+      if (error.code === '23505')
         throw new ConflictException('E-mail já cadastrado');
-      }
+
       throw error;
     }
   }
 
-  findAll() {
-    return `This action returns all person`;
+  async findAll() {
+    const person = await this.personRepository.find({ order: { id: 'desc' } });
+
+    return person;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} person`;
+  async findOne(id: number) {
+    const person = await this.personRepository.findOneBy({ id });
+
+    if (!person) this.notFoundException();
+
+    return person;
   }
 
-  update(id: number, updatePersonDto: UpdatePersonDto) {
-    return `This action updates a #${id} person`;
+  async update(id: number, updatePersonDto: UpdatePersonDto) {
+    const personData = {
+      name: updatePersonDto.name,
+      passwordHash: updatePersonDto.password,
+    };
+
+    const person = await this.personRepository.preload({ id, ...personData });
+
+    if (!person) this.notFoundException();
+
+    return this.personRepository.save(person);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} person`;
+  async remove(id: number) {
+    const person = await this.personRepository.findOneBy({ id });
+
+    if (!person) this.notFoundException();
+
+    return this.personRepository.remove(person);
   }
 }
