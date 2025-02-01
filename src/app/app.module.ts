4a0195a91b2625d8710f5c1b8b6109aed410f513
Gabriel Fernando <gf313074@gmail.com>
2025-02-01 00:10:49 -0300

    create validation schema to env config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,23 @@ import { MessageModule } from 'src/messages/message.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PersonModule } from 'src/person/person.module';
 import { ConfigModule } from '@nestjs/config';
+import * as Joi from '@hapi/joi';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ envFilePath: '.env.development' }),
+    ConfigModule.forRoot({
+      envFilePath: '.env.development.local',
+      validationSchema: Joi.object({
+        DATABASE_TYPE: Joi.required(),
+        DATABASE_HOST: Joi.required(),
+        DATABASE_PORT: Joi.number().default(5432),
+        DATABASE_USERNAME: Joi.required(),
+        DATABASE_DATABASE: Joi.required(),
+        DATABASE_PASSWORD: Joi.required(),
+        DATABASE_AUTOLOADENTITIES: Joi.boolean().default(false),
+        DATABASE_SYNCHRONIZE: Joi.boolean().default(false),
+      }),
+    }),
     TypeOrmModule.forRoot({
       type: process.env.DATABASE_TYPE as 'postgres',
       host: process.env.DATABASE_HOST,
